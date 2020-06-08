5261d7954066b27bf4957b2403ee04ee627e4a84
Ngo Van Thien <thienyl6716@gmail.com>
2020-06-08 18:15:06 +0700

    Updates

---
+++ precache-manifest.1cc2701e2007f49bdae530df1b5a7b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f129496c92859251a7715239a1f6900",
+    "url": "/homedey/index.html"
+  },
+  {
+    "revision": "3ed9d72197b5ab54a2b8",
+    "url": "/homedey/static/css/2.aee1d548.chunk.css"
+  },
+  {
+    "revision": "bb69be1b3ab64c13363d",
+    "url": "/homedey/static/css/main.8c7d72d9.chunk.css"
+  },
+  {
+    "revision": "3ed9d72197b5ab54a2b8",
+    "url": "/homedey/static/js/2.be790435.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/homedey/static/js/2.be790435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb69be1b3ab64c13363d",
+    "url": "/homedey/static/js/main.1e04c4ff.chunk.js"
+  },
+  {
+    "revision": "6111f8903e8d0df7f86b",
+    "url": "/homedey/static/js/runtime-main.0930b52c.js"
+  }
+]);
